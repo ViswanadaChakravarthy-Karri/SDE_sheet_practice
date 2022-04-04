bcbae03582dcb70d4d28079cde4655d84375beb4
viswanadhchakravarthi <47515524+viswanadhchakravarthi@users.noreply.github.com>
2022-04-05 01:19:43 +0530

    Update 5.1. Grid Unique Paths.cpp

--- day3-Arrays3/5.1. Grid Unique Paths.cpp
+++ day3-Arrays3/5.1. Grid Unique Paths.cpp
@@ -10,7 +10,8 @@ public:
         // try to make some observations ans is Combination( m-1 + n-1 , n-1 or m-1 )
         double result=1;
         for(int i=1 ; i<=r ; i++)
-            result = result* (N-r+i) / i;
+            result = result* (N-r+i) / i; 
+            // the only diff with below one is multiplication of numerator is done from smaller end.
         // result = result* (N-i+1) / i; // --> but why this is not working, even though looking same 😒
       // what kind of difference does it make
       //  its working in GDBC compiler but not LeetCode
