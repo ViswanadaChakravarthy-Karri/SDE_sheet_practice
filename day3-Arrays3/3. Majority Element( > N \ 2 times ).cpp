ea9c110a28cfa8382c6766418939d6e44d321778
viswanadhchakravarthi <47515524+viswanadhchakravarthi@users.noreply.github.com>
2022-04-04 20:52:13 +0530

    Create 3. Majority Element( > N \ 2 times ).cpp

---
+++ day3-Arrays3/3. Majority Element( > N \ 2 times ).cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+      
+    // Moore's Voting Algorithm
+    int majorityElement(vector<int>& nums) {
+        int count=0, maj;
+        
+      
+        for(int i=0; i<nums.size() ; i++){
+            // we are assuming the starting element as Maj , if count is 0
+            if(count==0){
+                // bcz we assume, before this count(maj) is balanced with count(others)
+                maj=nums[i];
+                count=1;
+            }
+            else{
+                if(nums[i]==maj) count ++;
+                // we increasing the count of Maj element
+                else count--;
+                // Balancing the count of majority elements with Other elements.
+            }
+        }
+        return maj;
+    }
+};
+// Time Complexity: O(n) Space Complexity : O(1)
+// Main intuition behind this algorithm is Majority element occurs >floor(N/2) times , where N: size of arr
