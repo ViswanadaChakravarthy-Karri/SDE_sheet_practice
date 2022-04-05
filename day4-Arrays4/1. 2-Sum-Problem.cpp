d77a28b8ed4570326c31b88003e14cf347c62406
viswanadhchakravarthi <47515524+viswanadhchakravarthi@users.noreply.github.com>
2022-04-05 12:36:55 +0530

    Update 1. 2-Sum-Problem.cpp

--- day4-Arrays4/1. 2-Sum-Problem.cpp
+++ day4-Arrays4/1. 2-Sum-Problem.cpp
@@ -1,1 +1,23 @@
-
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans{0,0};
+        unordered_map<int,int> um;
+        for(int i=0 ; i<nums.size() ; i++){
+            if(um[target-nums[i]]!=0){
+                ans[0] = i;
+                ans[1] = um[target-nums[i]];
+                if(ans[1]==-1) ans[1] = 0;
+            }
+            else if(um[nums[i]]==0){
+                um[nums[i]]=i;
+                if(i==0) um[nums[i]] = -1;
+            }   
+        }
+        return ans;
+    }
+};
+// Time complexity: O(n) , Space Complexity: O(n)
+// in Worst case unordered_map doesn't work in O(1)
+// then go with ordered_map --> which take O(log n),
+// then Total Complexity would become O(n logn)
