d7b5c29cc1a90f88fbc917a41d74d8b0a89f7700
viswanadhchakravarthi <47515524+viswanadhchakravarthi@users.noreply.github.com>
2022-04-09 17:58:41 +0530

    Create 2. Find the middle of LinkedList .cpp

---
+++ day5-LinkedList1/2. Find the middle of LinkedList .cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        if(head == NULL | head->next == NULL) return head;
+        
+        ListNode *slow=head,*fast=head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
+    }
+};
+// Time comp: O(n) Space comp: O(1)
+
+// Dry run:
+// NULL (ans:NULL)
+// 1->  (ans:1)
+// 1->2-> (ans:2)
+
+// 1->2->3->4->5->NULL
+// 1k 2k 3k 4k 5k
+// slow=3k
+// fast=5k
