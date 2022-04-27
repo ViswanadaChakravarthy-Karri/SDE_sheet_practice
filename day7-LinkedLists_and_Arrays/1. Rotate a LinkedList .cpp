904ab94b97be04a6c7eca006323e49b72991b9b3
viswanadhchakravarthi <47515524+viswanadhchakravarthi@users.noreply.github.com>
2022-04-27 08:55:39 +0530

    Update 1. Rotate a LinkedList .cpp

--- day7-LinkedLists_and_Arrays/1. Rotate a LinkedList .cpp
+++ day7-LinkedLists_and_Arrays/1. Rotate a LinkedList .cpp
@@ -1,1 +1,25 @@
-
+class Solution {
+public:
+    int length(ListNode* start){
+        int count=0;
+        while(start!=NULL) 
+            start=start->next, count++;
+        return count;
+    }
+    ListNode* rotateRight(ListNode* head, int k) {
+        int len=length(head);
+        if(len<=1 || k==0)
+            return head;
+        
+        int fdist= len - (k % len);
+        ListNode *tmp=head;
+        while(tmp->next != NULL) 
+            tmp=tmp->next;
+        tmp->next=head;
+        while(fdist--)
+            tmp=tmp->next;
+        head=tmp->next;
+        tmp->next=NULL;
+        return head;
+    }
+};
