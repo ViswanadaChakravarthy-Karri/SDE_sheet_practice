2805bfe928a528b189ca5783a814d76fad4866ec
viswanadhchakravarthi <47515524+viswanadhchakravarthi@users.noreply.github.com>
2022-04-27 16:04:02 +0530

    Create 3. 3 sum .cpp

---
+++ day7-LinkedLists_and_Arrays/3. 3 sum .cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void removeDuplicates(vector<int>& nums, int &ptr, bool l_to_r){
+        if(l_to_r) while(ptr+1<nums.size() && nums[ptr]==nums[ptr+1]) ptr++;
+        else       while(ptr-1>=0 && nums[ptr]==nums[ptr-1]) ptr--;
+    }
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int sum;
+        int n=nums.size();
+        vector<vector<int>> ans;
+        if(n<3) 
+            return ans;
+        sort(nums.begin(),nums.end());
+        for(int j=0 ; j<n ; j++){
+            int rem = 0-nums[j], k = j+1, l=nums.size()-1;
+                
+                while(k < l){
+                    if( nums[k]+nums[l]==rem ){
+                        ans.push_back({nums[j],nums[k],nums[l]});
+                        removeDuplicates(nums,k,true),  k++;
+                        removeDuplicates(nums,l,false), l--;
+                    }
+                    else if( nums[k]+nums[l] < rem) 
+                        removeDuplicates(nums,k,true),  k++;
+                    else                                 
+                        removeDuplicates(nums,l,false), l--;
+                } 
+                // jumping over duplicates
+                removeDuplicates(nums,j,true);
+        }
+        return ans;
+    }
+};
+// Time: O(n^2) , Space: O(1)
