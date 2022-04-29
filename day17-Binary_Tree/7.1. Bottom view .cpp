2f2de079bef51db6e456ca57699b7ae8865a257a
viswanadhchakravarthi <47515524+viswanadhchakravarthi@users.noreply.github.com>
2022-04-29 23:58:51 +0530

    Update 7.1. Bottom view .cpp

--- day17-Binary_Tree/7.1. Bottom view .cpp
+++ day17-Binary_Tree/7.1. Bottom view .cpp
@@ -1,1 +1,27 @@
-
+class Solution {
+  public:
+    vector<int> bottomView(Node *root) {
+        vector<int> res;
+        if(root==NULL)
+            return res;
+        map<int,int> mp;  //<index,value>  
+        queue<pair<Node*,int>> q;
+        q.push({root,0});
+        while(!q.empty()){
+            auto pr=q.front();
+            q.pop();
+            Node *curr=pr.first;
+            int index=pr.second;
+            mp[index]=curr->data;
+            if(curr->left)
+                q.push({curr->left,index-1});
+            if(curr->right)
+                q.push({curr->right,index+1});
+        }
+        for(auto ele:mp){
+            res.push_back(ele.second);
+        }
+        return res;
+    }
+};
+// O(nlogn) time, O(n) space
