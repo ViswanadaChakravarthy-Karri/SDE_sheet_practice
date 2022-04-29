d104fdd58062007dcfaf34a8405e7d0f78d70655
viswanadhchakravarthi <47515524+viswanadhchakravarthi@users.noreply.github.com>
2022-04-29 13:15:38 +0530

    Create 7.0. Bottom view( not working ) .cpp

---
+++ day17-Binary_Tree/7.0. Bottom view( not working ) .cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    void findBottomView(Node *root,int ind,map<int,int> &mp){
+        if(root==NULL) 
+            return;
+        
+        mp[ind]=root->data;
+        
+        findBottomView(root->left,ind-1,mp);
+        findBottomView(root->right,ind+1,mp);
+    }
+    vector<int> bottomView(Node *root) {
+        map<int,int> mp;
+        findBottomView(root,0,mp);
+        vector<int> res;
+        for(auto i:mp)
+            res.push_back(i.second);
+        return res;
+    }
+};
+// why not working......???☹️
