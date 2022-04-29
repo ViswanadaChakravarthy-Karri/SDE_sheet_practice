93664adacbaac21ed4b9c2e10382df9c3f180882
viswanadhchakravarthi <47515524+viswanadhchakravarthi@users.noreply.github.com>
2022-04-29 07:17:55 +0530

    Create 2.1. Preorder Traversal( Iterative ) .cpp

---
+++ day17-Binary_Tree/2.1. Preorder Traversal( Iterative ) .cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    // +ab
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> res;
+        if(root==NULL)
+            return res;
+        stack<TreeNode*> st;
+        st.push(root);
+        while(!st.empty()){
+            root=st.top();
+            st.pop();
+            res.push_back(root->val);
+            if(root->right)
+                st.push(root->right);
+            if(root->left)
+                st.push(root->left);
+        }
+        return res;
+    }
+};
+// time: O(#nodes) , space: O(H)
