7e000375128c08b210064a1eacb08ffc482b9fd1
viswanadhchakravarthi <47515524+viswanadhchakravarthi@users.noreply.github.com>
2022-05-16 16:15:40 +0530

    Update 11. Root to node path in a Binary Tree .cpp

--- day17-Binary_Tree/11. Root to node path in a Binary Tree .cpp
+++ day17-Binary_Tree/11. Root to node path in a Binary Tree .cpp
@@ -1,20 +1,46 @@
-bool ancestors(TreeNode* root,int node,vector<int> &res)
+class Solution1
 {
-    if(root==NULL)
+  public:
+      bool ancestors(TreeNode* root,int node,vector<int> &res)
+      {
+        if(root==NULL)
+            return false;
+        if(root->val == node){
+            res.push_back(root->val);
+            return true;
+        }
+        if( ancestors(root->left,node,res) || ancestors(root->right,node,res) ){
+            res.push_back(root->val);
+            return true;
+        }
         return false;
-    if(root->val == node){
-        res.push_back(node);
-        return true;
-    }
-    if( ancestors(root->left,node,res) || ancestors(root->right,node,res) ){
+      }
+      vector<int> Solution::solve(TreeNode* A, int B) {
+        vector<int> res;
+        ancestors(A,B,res);
+        reverse(res.begin(),res.end());
+        return res;
+      }
+};
+// time : O(#nodes) for both solution 
+class Solution2
+{
+  public:
+    bool getPath(TreeNode* root,int node,vector<int> &res)
+    {
+        if(root==NULL)
+            return false;
         res.push_back(root->val);
-        return true;
+        if(root->val == node)
+            return true;
+        if( getPath(root->left,node,res) || getPath(root->right,node,res) )
+            return true;
+        res.pop_back();
+        return false;
     }
-    return false;
-}
-vector<int> Solution::solve(TreeNode* A, int B) {
-    vector<int> res;
-    ancestors(A,B,res);
-    reverse(res.begin(),res.end());
-    return res;
-}
+    vector<int> Solution::solve(TreeNode* A, int B) {
+        vector<int> res;
+        getPath(A,B,res);
+        return res;
+    }
+};
