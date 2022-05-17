380ab4d852089afc1e4ceaa2b7d74c80cfe760c5
viswanadhchakravarthi <47515524+viswanadhchakravarthi@users.noreply.github.com>
2022-05-17 23:01:50 +0530

    Update 2. Minimum number of platforms required for a railway .cpp

--- day8-Greedy_Algorithms/2. Minimum number of platforms required for a railway .cpp
+++ day8-Greedy_Algorithms/2. Minimum number of platforms required for a railway .cpp
@@ -1,4 +1,4 @@
-class Solution_1{
+class Solution_2{
     public:
     //Function to find the minimum number of platforms required at the
     //railway station such that no train waits.
@@ -32,3 +32,35 @@ class Solution_1{
 };
 
 
+class Solution_3{
+    public:
+    //Function to find the minimum number of platforms required at the
+    //railway station such that no train waits.
+    int std_timing(int time)
+    {
+        if(time%100 >= 60)
+        {
+            time += 40; // -60 +100
+        }
+        return time;
+    }
+    int findPlatform(int arr[], int dep[], int n)
+    {
+        int mpp[2361]={0};
+        for(int j=0;j<n;j++)
+        {
+            mpp[std_timing(arr[j])]++;
+            mpp[std_timing(dep[j]+1)]--;
+        }
+        // min_platforms_req to solve problm is max_#platforms_used at any PointOfTime.
+        int max_no_of_platforms_used=0,curr_platform_in_use=0;
+        for(int k=0;k<2361;k++)
+        {
+            curr_platform_in_use += mpp[k];
+            max_no_of_platforms_used = max (max_no_of_platforms_used,curr_platform_in_use);
+        }
+        return max_no_of_platforms_used;
+    }
+    // Time Complexity: O(n) bcz we are using direct hashing
+    // Auxiliary Space: O(1) auxilary space will always req following size ( 0000 ≤ A[i] ≤ D[i] ≤ 2359 )
+};
