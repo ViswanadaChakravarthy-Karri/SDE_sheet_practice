d631c2a9692a2e28b797e6fe269de513e589b77b
viswanadhchakravarthi <47515524+viswanadhchakravarthi@users.noreply.github.com>
2022-05-18 21:51:28 +0530

    Update 5. Greedy algorithm to find minimum number of coins .cpp

--- day8-Greedy_Algorithms/5. Greedy algorithm to find minimum number of coins .cpp
+++ day8-Greedy_Algorithms/5. Greedy algorithm to find minimum number of coins .cpp
@@ -38,3 +38,21 @@ class Solution{
 // 	Time: The time complexity of the above solution is O(mV).
 // 	Space: O(V)
 };
+
+
+Question2:
+	  int V = 49;
+	  vector < int > ans;
+	  int coins[] = {1, 2, 5, 10, 20, 50, 100, 500, 1000};
+	  int n = 9;
+	  for (int i = n - 1; i >= 0; i--) {
+	    while (V >= coins[i]) {
+	      V -= coins[i];
+	      ans.push_back(coins[i]);
+	    }
+	  }
+	  cout<<"The minimum number of coins is "<<ans.size()<<endl;
+	  cout<<"The coins are "<<endl;
+	  for (int i = 0; i < ans.size(); i++) {
+	    cout << ans[i] << " ";
+ 	  }
