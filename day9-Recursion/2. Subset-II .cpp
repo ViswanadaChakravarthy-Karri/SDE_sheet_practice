4808479bf4c179e11c51b583a0879450ba3a3829
viswanadhchakravarthi <47515524+viswanadhchakravarthi@users.noreply.github.com>
2022-05-23 18:18:49 +0530

    Update 2. Subset-II .cpp

--- day9-Recursion/2. Subset-II .cpp
+++ day9-Recursion/2. Subset-II .cpp
@@ -1,3 +1,17 @@
+// Solution 1: Brute Force
+
+// Approach:
+// At every index, we make a decision whether to pick or not pick the element at that index. 
+// This will help us in generating all possible combinations but does not take care of the duplicates. 
+// Hence we will use a set to store all the combinations that will discard the duplicates.
+
+// Time Complexity: O( 2^n *(k log (x) )). 2^n  for generating every subset and k* log( x)  to insert every combination of average length k in a set of size x. 
+//                  After this, we have to convert the set of combinations back into a list of list /vector of vectors which takes more time.
+
+// Space Complexity:  O(2^n * k) to store every subset of average length k. Since we are initially using a set to store the answer another O(2^n *k) is also used.
+   
+// Solution 2: Optimal
+
 class Solution {
 private:
     void findSubsets(int ind, vector<int>& nums, vector<int>& ds, vector<vector<int>>& ans) {
