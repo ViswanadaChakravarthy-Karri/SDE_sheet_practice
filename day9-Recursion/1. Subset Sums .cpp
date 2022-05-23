3ba837f49b38ea99c7c36ac8241fe01e8153a4b6
viswanadhchakravarthi <47515524+viswanadhchakravarthi@users.noreply.github.com>
2022-05-23 12:05:30 +0530

    Update 1. Subset Sums .cpp

--- day9-Recursion/1. Subset Sums .cpp
+++ day9-Recursion/1. Subset Sums .cpp
@@ -1,23 +1,28 @@
 class Solution
 {
 public:
-    void solveSS(vector<int>& arr,int N,vector<int>& res,int sum=0)
+    void func(vector<int>& arr,int N,vector<int>& sumSubset,int sum)
     {
         if(N<0)
         {
-            res.push_back(sum);
+            sumSubset.push_back(sum);
             return;
         }
-        solveSS(arr,N-1,res,sum+arr[N]);
-        solveSS(arr,N-1,res,sum);
+        // pick-up the curr index
+        func(arr,N-1,sumSubset,sum+arr[N]);
+        
+        // do not pick-up the curr index
+        func(arr,N-1,sumSubset,sum);
+        
         return;
     }
     vector<int> subsetSums(vector<int> arr, int N)
     {
-        vector<int> res;
-        solveSS(arr,N-1,res);
-        return res;
+        vector<int> sumSubset;
+        func(arr,N-1,sumSubset,0);
+        sort(sumSubset.begin(),sumSubset.end());
+        return sumSubset;
     }
 };
-// Expected Time Complexity: O(2^N)
-// Expected Auxiliary Space: O(2^N)
+// Time: O(2^N) for Recursion-calls + O( 2^N * log(2^N) ) for sorting
+// Space: O(2^N) for sumSubset + O(N) for Recursion calls
